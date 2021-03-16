6dc27a5312350702a628d208ba49d799aa4ce06d
Caio Ortu <caio.ortu@avenuecode.com>
2021-03-16 17:22:49 -0300

    Load feed on pull to refresh

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -9,28 +9,34 @@ import XCTest
 import UIKit
 import EssentialFeed
 
-final class FeedViewController: UIViewController {
-     private var loader: FeedLoader?
-
-     convenience init(loader: FeedLoader) {
-         self.init()
-         self.loader = loader
-     }
-
-     override func viewDidLoad() {
-         super.viewDidLoad()
-
+final class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
         loader?.load { _ in }
-     }
- }
+    }
+}
 
- final class FeedViewControllerTests: XCTestCase {
-
-     func test_init_doesNotLoadFeed() {
+final class FeedViewControllerTests: XCTestCase {
+    
+    func test_init_doesNotLoadFeed() {
         let (_, loader) = makeSUT()
-
-         XCTAssertEqual(loader.loadCallCount, 0)
-     }
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
     
     func test_viewDidLoad_loadsFeed() {
         let (sut, loader) = makeSUT()
@@ -39,8 +45,21 @@ final class FeedViewController: UIViewController {
         
         XCTAssertEqual(loader.loadCallCount, 1)
     }
-
-     // MARK: - Helpers
+    
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        sut.refreshControl?.allTargets.forEach { target in
+            sut.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+        
+        XCTAssertEqual(loader.loadCallCount, 2)
+    }
+    
+    // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
@@ -49,13 +68,12 @@ final class FeedViewController: UIViewController {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
     }
-     
+    
     class LoaderSpy: FeedLoader {
-         private(set) var loadCallCount: Int = 0
+        private(set) var loadCallCount: Int = 0
         
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             loadCallCount += 1
         }
-     }
-
- }
+    }
+}
